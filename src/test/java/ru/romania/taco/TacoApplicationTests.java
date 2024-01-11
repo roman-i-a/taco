634518c38b93b4f9c65f53532305ee6d7dd37ac5
Авилов Роман Игоревич <aviloff.rom@yandex.ru>
2024-01-11 17:53:20 +0300

    Initial commit

---
+++ src/test/java/ru/romania/taco/TacoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romania.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
