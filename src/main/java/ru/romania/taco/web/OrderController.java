6ec305bf2a7ed5cc7eafd2a674edc3a1cd7fea62
Авилов Роман Игоревич <aviloff.rom@yandex.ru>
2024-01-16 16:09:47 +0300

    implemented /orders/current

--- src/main/java/ru/romania/taco/web/OrderController.java
+++ src/main/java/ru/romania/taco/web/OrderController.java
@@ -3,9 +3,12 @@ package ru.romania.taco.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.romania.taco.TacoOrder;
 
 
 @Slf4j
@@ -18,5 +21,11 @@ public class OrderController {
     public String orderForm() {
         return "orderForm";
     }
-    
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
 }
