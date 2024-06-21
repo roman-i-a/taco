879e0f592dd7a3906b844178ba911e2532f11feb
Авилов Роман Игоревич <aviloff.rom@yandex.ru>
2024-06-21 10:05:35 +0300

    define CrudRepository interfaces

--- src/main/java/ru/romania/taco/data/IngredientRepository.java
+++ src/main/java/ru/romania/taco/data/IngredientRepository.java
@@ -1,15 +1,8 @@
 package ru.romania.taco.data;
 
-import java.util.Optional;
-
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import ru.romania.taco.Ingredient;
 
-public interface IngredientRepository extends Repository<Ingredient, String> {
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
--- src/main/java/ru/romania/taco/data/OrderRepository.java
+++ src/main/java/ru/romania/taco/data/OrderRepository.java
@@ -1,9 +1,8 @@
 package ru.romania.taco.data;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import ru.romania.taco.TacoOrder;
 
-public interface OrderRepository extends Repository<TacoOrder, String> {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }
