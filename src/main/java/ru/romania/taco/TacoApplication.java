742f5788f5ce638feb0c4fdd10fe0c6e944f801e
roman-i-a <aviloff.rom@yandex.ru>
2024-06-23 18:54:12 +0300

    add CommandLineRunner dataLoad

--- src/main/java/ru/romania/taco/TacoApplication.java
+++ src/main/java/ru/romania/taco/TacoApplication.java
@@ -1,8 +1,15 @@
 package ru.romania.taco;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+import ru.romania.taco.Ingredient.Type;
+import ru.romania.taco.data.IngredientRepository;
+
+@Slf4j
 @SpringBootApplication
 public class TacoApplication {
 
@@ -10,4 +17,12 @@ public class TacoApplication {
 		SpringApplication.run(TacoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(IngredientRepository ingredientRepo) {
+		log.info("CommandLineRunner started.");
+		return args -> {
+			ingredientRepo.save(new Ingredient("FRCH", "Fried Chicken", Type.PROTEIN));
+			ingredientRepo.save(new Ingredient("KAIK", "Russian Kabachok Ikra", Type.VEGGIES));
+		};
+	}
 }
