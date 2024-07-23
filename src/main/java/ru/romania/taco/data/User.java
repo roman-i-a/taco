c89c5bb2831ad64f705f16ad15ba81418a9ee782
Авилов Роман Игоревич <aviloff.rom@yandex.ru>
2024-07-23 11:43:06 +0300

    bug fix, change table name of User entity

--- src/main/java/ru/romania/taco/data/User.java
+++ src/main/java/ru/romania/taco/data/User.java
@@ -12,15 +12,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-@Entity(name = "my_user")
+@Entity(name = "taco_user")
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-@RequiredArgsConstructor
 public class User implements UserDetails {
 
     private static final long serialVersionUID = 1L;
@@ -29,14 +26,14 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String username;
-    private String password;
-    private String fullname;
-    private String street;
-    private String city;
-    private String state;
-    private String zip;
-    private String phoneNumber;
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -63,4 +60,6 @@ public class User implements UserDetails {
         return true;
     }
 
+    
+    
 }
